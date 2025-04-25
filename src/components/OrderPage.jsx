6c74618dd616ab8da45eebf0a9d7eded04a36439
ubing <ubing0101@kakao.com>
2025-04-25 10:30:27 +0900

    axios를 통한 백엔드 요청 보내기, 주문 요청 생성, MyPage 컴포넌트 추가 및 요청 생성

--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -13,6 +13,7 @@ import {
 } from '@mui/material';
 import React, { useContext } from 'react';
 import CartContext from '../context/CartContext';
+import axios from 'axios';
 
 const OrderPage = () => {
   const { productsInCart, clearCart: onClear } = useContext(CartContext);
@@ -21,7 +22,60 @@ const OrderPage = () => {
     onClear();
   };
 
-  const orderCreate = () => {};
+  const orderCreate = async () => {
+    // 백엔드가 달라는 형태로 줘야하니까 그에 맞게 객체를 매핑
+    const orderProducts = productsInCart.map((p) => ({
+      productId: p.id,
+      productQuantity: p.quantity,
+    }));
+
+    if (orderProducts.length < 1) {
+      alert('주문 대상 물품이 없습니다.');
+      return;
+    }
+
+    const yesOrNo = confirm(
+      `${orderProducts.length}개의 상품을 주문하시겠습니까?`,
+    );
+
+    if (!yesOrNo) {
+      alert('주문이 취소되었습니다.');
+      return;
+    }
+
+    // const res = await fetch('http://localhost:8181/order/create', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json;cahrset=UTF-8',
+    //     Authorization: 'Bearer ' + LocalStorage.getItem('ACCESS_TOKEN'),
+    //   },
+    //   body: JSON.stringify(orderProducts),
+    // });
+
+    // axios 를 이용한 백엔드 요청
+    // axios는 요청 방식에 따라 메서드를 제공함.
+    // (url, 전달하고자 하는 데이터(JSON으로 직접 변경 x), 헤더정보)
+    // axios는 200번대 정상 응답이 아닌 모든 것을 예외로 처리하기 때문에
+    // try,cathc로 작성해야한다. (fetch는 5400번대 응답에도 예외가 발생하)
+    try {
+      const res = await axios.post(
+        'http://localhost:8181/order/create',
+        orderProducts,
+        {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('ACCESS_TOKEN'),
+          },
+        },
+      );
+      // const data = res.json(); -> fetch를 사용했을땐 데이터를 꺼내는 과정이 있음
+      // console.log('axios를 통해 전달받은 데이터: ', res);
+      alert('주문이 완료되었습니다.');
+      clearCart();
+    } catch (err) {
+      console.log(err);
+      alert('주문 실패');
+    }
+  };
 
   return (
     <Container>
