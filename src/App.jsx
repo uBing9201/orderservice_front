573dc4153a310af23fb9ab45d982840d172fd315
ubing <ubing0101@kakao.com>
2025-04-28 14:07:20 +0900

    Router 분리, 인증 및 권한을 일괄 확인할 수 있는 Private Router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
-import Home from './components/Home';
 import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
-import MemberCreate from './components/MemberCreate';
 import { AuthContextProvider } from './context/UserContext';
-import LoginPage from './components/LoginPage';
 import './App.css';
-import ProductList from './components/ProductList';
 import { CartContextProvider } from './context/CartContext';
-import OrderPage from './components/OrderPage';
-import MyPage from './components/MyPage';
-import ProductCreate from './components/ProductCreate';
+import AppRouter from './router/AppRouter';
 
 const App = () => {
   return (
@@ -20,15 +13,7 @@ const App = () => {
         <div className='App'>
           <Header />
           <div className='content-wrapper'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/member/create' element={<MemberCreate />} />
-              <Route path='/login' element={<LoginPage />} />
-              <Route path='/product/list' element={<ProductList />} />
-              <Route path='/order/cart' element={<OrderPage />} />
-              <Route path='/mypage' element={<MyPage />} />
-              <Route path='/product/create' element={<ProductCreate />} />
-            </Routes>
+            <AppRouter />
           </div>
           <Footer />
         </div>
