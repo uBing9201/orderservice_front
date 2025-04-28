299875e056134952b39cd22a5f4aea93a45d52ce
ubing <ubing0101@kakao.com>
2025-04-28 11:53:14 +0900

    검색 기능 구현. 검색 시 기존 렌더링 내용 초기화 후 요청 보내기

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -31,22 +31,20 @@ const ProductList = ({ pageTitle }) => {
   const [isLastPage, setLastPage] = useState(false); // 마지막 페이지 여부
   // 현재 로딩중이냐? -> 백엔드로부터 상품 목록 요청을 보내서 아직 데이터를 받아오는 중인가?
   const [isLoading, setIsLoading] = useState(false);
-  const pageSize = 15; // 한 페이지당 노출시킬 아이템 수
+  const pageSize = 15;
 
   const { userRole } = useContext(AuthContext);
   const isAdmin = userRole === 'ADMIN';
 
   const { addCart } = useContext(CartContext);
-
   const navigate = useNavigate();
 
   useEffect(() => {
     loadProduct(); // 처음 화면에 진입하면 1페이지 내용을 불러오자. (매개값은 필요 없음)
     // 쓰로틀링: 짧은 시간동안 연속해서 발생한 이벤트들을 일정 시간으로 그룹화 하여
-    // 마지막 이벤트 핸들러만 호출하게 하는 기능. -> 스크롤 페이징
+    // 순차적으로 적용할 수 있게 하는 기법 -> 스크롤 페이징
     // 디바운싱: 짧은 시간동안 연속해서 발생한 이벤트를 호출하지 않다가 마지막 이벤트로부터
     // 일정 시간 이후에 한번만 호출하게 하는 기능. -> 입력값 검증
-
     const throttledScroll = throttle(scrollPagination, 1000);
     window.addEventListener('scroll', throttledScroll);
 
@@ -57,28 +55,39 @@ const ProductList = ({ pageTitle }) => {
   useEffect(() => {
     // useEffect는 하나의 컴포넌트에서 여러 개 선언이 가능.
     // 스크롤 이벤트에서 다음 페이지 번호를 준비했고,
-    // 상태가 바뀌면 그 때 백엔드로 요청을 보낼 수 있게 로직 분리
+    // 상태가 바뀌면 그 때 백엔드로 요청을 보낼 수 있게 로직을 나누었습니다.
     if (currentPage > 0) loadProduct();
   }, [currentPage]);
 
   // 상품 목록을 백엔드에 요청하는 함수
   const loadProduct = async () => {
-    // 아직 로딩중 이라면 or 이미 마지막 페이지라면 더이상 진행하지 말아라
+    // 아직 로딩 중이라면 or 이미 마지막 페이지라면 더이상 진행하지 말어라.
     if (isLoading || isLastPage) return;
+
+    console.log('아직 보여줄 컨텐트 더 있음!');
 
     const params = {
       size: pageSize,
       page: currentPage,
     };
 
+    // 만약 사용자가 조건을 선택했고, 검색어를 입력했다면 프로퍼티를 추가하자.
+    if (searchType !== 'optional' && searchValue) {
+      params.category = searchType;
+      params.searchName = searchValue;
+    }
+
+    console.log('백엔드로 보낼 params: ', params);
+
     setIsLoading(true); // 요청 보내기 바로 직전에 로딩 상태 true 만들기
 
-    const res = await axios.get('http://localhost:8181/product/list', {
-      params,
-    });
-    const data = await res.data;
-
     try {
+      const res = await axios.get('http://localhost:8181/product/list', {
+        params,
+      });
+      const data = await res.data;
+      console.log('result.length: ', data.result.length);
+
       if (data.result.length === 0) {
         setLastPage(true);
       } else {
@@ -109,12 +118,11 @@ const ProductList = ({ pageTitle }) => {
   const handleAddToCart = () => {
     // 특정 객체에서 key값만 뽑아서 문자열 배열로 리턴해 주는 메서드
     const selectedProduct = Object.keys(selected);
-    // console.log(selectedProduct);
 
-    // key값만 뽑아서 selected에 들어있는 상품 중에 false 다 빼고 true인 것만 담기
+    // key값만 뽑아서 selected에 들어있는 상품 중에 false 다 빼고 true인 것만 담겠다.
     const filtered = selectedProduct.filter((key) => selected[key]);
 
-    // 사용자가 선택한 수량까지 파악해서 장바구니에 넣어 줍니다.
+    // 사용자가 선택한 수량까지 파악해서 장바구니에 넣어 줍시다.
     const finalSelected = filtered.map((key) => {
       const product = productList.find((p) => p.id === parseInt(key));
       return {
@@ -123,8 +131,7 @@ const ProductList = ({ pageTitle }) => {
         quantity: product.quantity,
       };
     });
-
-    // console.log(finalSelected);
+    console.log(finalSelected);
 
     if (finalSelected.length < 1) {
       alert('장바구니에 추가할 상품을 선택해 주세요!');
@@ -139,27 +146,24 @@ const ProductList = ({ pageTitle }) => {
     }
 
     if (confirm('상품을 장바구니에 추가하시겠습니까?')) {
-      // 카트로 상품을 보내주기
-      // addCart(finalSelected);
+      // 카트로 상품을 보내주자. (addCart에는 상품을 하나씩 보내세요.)
       finalSelected.forEach((product) => addCart(product));
       alert('선택한 상품이 장바구니에 추가되었습니다.');
     }
 
-    if (confirm('장바구니로 이동하시겠습니까?')) {
+    if (confirm('장바구니 화면으로 이동할까요?')) {
       navigate('/order/cart');
     }
   };
 
   // 체크박스 클릭 이벤트 핸들러
   const handleCheckboxChange = (productId, checked) => {
-    // 사용자가 특정 상품을 선택했는지에 대하 상태를 관리
+    // 사용자가 특정 상품을 선택했는지에 대한 상태를 관리
     setSelected((prev) => ({
       ...prev,
       [productId]: checked,
     }));
   };
-
-  // console.log(selected);
 
   return (
     <Container>
@@ -173,6 +177,11 @@ const ProductList = ({ pageTitle }) => {
           <form
             onSubmit={(e) => {
               e.preventDefault();
+              // 검색 렌더링 진행 시 기존 목록을 지우고 다시 렌더링 해야 해요!
+              setProductList([]);
+              setCurrentPage(0);
+              setIsLoading(false);
+              setLastPage(false);
               loadProduct();
             }}
           >
